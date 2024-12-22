6af6b250bfb7e74eaede0409201ba54fe56de072
chrishawnm <45526483+chrishawnm@users.noreply.github.com>
2024-12-22 09:34:43 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,15 +10,40 @@ async function fetchTitles(personId) {
         const data = await response.json();
         console.log(data);
         // Display the response in the UI
-        document.getElementById("output").textContent = `Titles: ${data.titles.join(", ")}`;
+        document.getElementById("output0").textContent = `Titles: ${data.titles.join(", ")}`;
     } catch (error) {
         console.error("Error fetching data:", error);
-        document.getElementById("output").textContent = "Error fetching data.";
+        document.getElementById("output0").textContent = "Error fetching data.";
+    }
+}
+
+
+async function fetchPaths(start-title) {
+    const apiUrl = `https://chrishawnmiles.pythonanywhere.com/paths/${start_title}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        // Display the response in the UI
+        document.getElementById("output1").textContent = `Paths: ${data.paths.join(", ")}`;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById("output1").textContent = "Error fetching data.";
     }
 }
 
 // Event listener for a button click
-document.getElementById("fetch-btn").addEventListener("click", () => {
+document.getElementById("fetch-btn0").addEventListener("click", () => {
     const personId = document.getElementById("person-id").value;
     fetchTitles(personId);
+
+// Event listener for a button click
+document.getElementById("fetch-btn1").addEventListener("click", () => {
+    const personId = document.getElementById("path-id").value;
+    fetchPaths(start-title);
 });
