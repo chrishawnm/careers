8831fdc1ef9e04c7172ae9a843cc2b89acbc52f3
chrishawnm <45526483+chrishawnm@users.noreply.github.com>
2024-12-23 13:36:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
+async function fetchpathfollowers(startTitle) {
+    const apiUrl = `https://chrishawnmiles.pythonanywhere.com/paths_with_followers/${starttitle}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        // Display the response in the UI
+        document.getElementById("output0").textContent = `Path: ${data.followers.join(", ")}`;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById("output0").textContent = "Error fetching data.";
+    }
+}
+
+
+async function fetchCurrenTtitle(titleName) {
+    const apiUrl = `https://chrishawnmiles.pythonanywhere.com/current_title/${titlename}`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+        // Display the response in the UI
+        // document.getElementById("output1").textContent = `People this as a current title: ${data.current_title.join(", ")}`;
+        document.getElementById("output1").innerHTML = `Paths: <br> ${data.paths.map(path => path.join(", ")).join("<br>")}`;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById("output1").textContent = "Error fetching data.";
+    }
+}
+
 async function fetchTitles(personId) {
     const apiUrl = `https://chrishawnmiles.pythonanywhere.com/titles/${personId}`;
 
@@ -10,42 +50,29 @@ async function fetchTitles(personId) {
         const data = await response.json();
         console.log(data);
         // Display the response in the UI
-        document.getElementById("output0").textContent = `Titles: ${data.titles.join(", ")}`;
+        document.getElementById("output2").textContent = `Their roles: ${data.titles.join(", ")}`;
     } catch (error) {
         console.error("Error fetching data:", error);
-        document.getElementById("output0").textContent = "Error fetching data.";
+        document.getElementById("output2").textContent = "Error fetching data.";
     }
 }
 
-
-async function fetchPaths(startTitle) {
-    const apiUrl = `https://chrishawnmiles.pythonanywhere.com/paths/${startTitle}`;
-
-    try {
-        const response = await fetch(apiUrl);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const data = await response.json();
-        console.log(data);
-        // Display the response in the UI
-        // document.getElementById("output1").textContent = `Paths: ${data.paths.join(", ")}`;
-        document.getElementById("output1").innerHTML = `Paths: <br> ${data.paths.map(path => path.join(", ")).join("<br>")}`;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        document.getElementById("output1").textContent = "Error fetching data.";
-    }
-}
 
 // Event listener for a button click
 document.getElementById("fetch-btn0").addEventListener("click", () => {
-    const personId = document.getElementById("person-id").value;
+    const startTitle = document.getElementById("current-title-id").value;
     fetchTitles(personId);
 });
 
 // Event listener for a button click
 document.getElementById("fetch-btn1").addEventListener("click", () => {
-    const startTitle = document.getElementById("path-id").value;
+    const titleName = document.getElementById("title-id").value;
+    fetchPaths(titleName);
+});
+
+// Event listener for a button click
+document.getElementById("fetch-btn2").addEventListener("click", () => {
+    const personId = document.getElementById("path-follower-id").value;
     fetchPaths(startTitle);
 });
+
